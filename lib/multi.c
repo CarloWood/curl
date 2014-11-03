128aeaf6a1b87119904610aca8694aade83e480d
Carlo Wood <carlo@alinoe.com>
2014-11-03 23:00:56 +0100

    Only assign NULL to data->easy_conn directly.
    This commits replaces occurances of *connp = NULL with
    data->easy_conn = NULL, adding DEBUGASSERT to make sure
    that this is allowed.
    
    What this comes down to is that we only want to set easy_conn
    to NULL when the connection is owned by the SessionHandle
    that contains that easy_conn. Thus:
    
      data->easy_conn = NULL;
    
    is only allowed while
    
      data->easy_conn->data == data
    
    This is a preparation for the next commit that will need this.
    
    Note that this commit also removes a duplicated variable
    in curl_multi_remove_handle (nl. 'easy') because it was
    an alias for 'data' and having two different variables for
    the exact same thing did not make the code better understandable.
    In particular, now we can read 'data->easy_conn->data == data'
    and 'data->easy_conn->data = data' which matches the above ;)
    
    Finally, in Curl_reconnect_request() we pass connp to
    Curl_done() instead of &conn (where conn is a local variable).
    The reason is that 'connp' fullfills the above assertion that
    Curl_done needs while '&conn' doesn't. There is not functional
    difference since 'conn' is not used below this call, *connp
    is assigned NULL below that call, and initially conn == *connp.

--- lib/multi.c
+++ lib/multi.c
@@ -485,8 +485,7 @@ CURLMcode curl_multi_remove_handle(CURLM *multi_handle,
                                    CURL *curl_handle)
 {
   struct Curl_multi *multi=(struct Curl_multi *)multi_handle;
-  struct SessionHandle *easy = curl_handle;
-  struct SessionHandle *data = easy;
+  struct SessionHandle *data = curl_handle;     /* easy handle */
   bool premature;
   bool easy_owns_conn;
   struct curl_llist_element *e;
@@ -505,7 +504,7 @@ CURLMcode curl_multi_remove_handle(CURLM *multi_handle,
 
 
   premature = (data->mstate < CURLM_STATE_COMPLETED) ? TRUE : FALSE;
-  easy_owns_conn = (data->easy_conn && (data->easy_conn->data == easy)) ?
+  easy_owns_conn = (data->easy_conn && (data->easy_conn->data == data)) ?
     TRUE : FALSE;
 
   /* If the 'state' is not INIT or COMPLETED, we might need to do something
@@ -526,7 +525,7 @@ CURLMcode curl_multi_remove_handle(CURLM *multi_handle,
     connclose(data->easy_conn, "Removed with partial response");
     /* Set connection owner so that Curl_done() closes it.
        We can safely do this here since connection is killed. */
-    data->easy_conn->data = easy;
+    data->easy_conn->data = data;
   }
 
   /* The timer must be shut down before data->multi is set to NULL,
@@ -573,7 +572,7 @@ CURLMcode curl_multi_remove_handle(CURLM *multi_handle,
   /* change state without using multistate(), only to make singlesocket() do
      what we want */
   data->mstate = CURLM_STATE_COMPLETED;
-  singlesocket(multi, easy); /* to let the application know what sockets that
+  singlesocket(multi, data); /* to let the application know what sockets that
                                 vanish with this handle */
 
   /* Remove the association between the connection and the handle */
@@ -590,7 +589,7 @@ CURLMcode curl_multi_remove_handle(CURLM *multi_handle,
   for(e = multi->msglist->head; e; e = e->next) {
     struct Curl_message *msg = e->ptr;
 
-    if(msg->extmsg.easy_handle == easy) {
+    if(msg->extmsg.easy_handle == data) {
       Curl_llist_remove(multi->msglist, e, NULL);
       /* there can only be one from this specific handle */
       break;
@@ -971,7 +970,8 @@ static CURLMcode multi_runsingle(struct Curl_multi *multi,
 
     if(data->easy_conn && data->mstate > CURLM_STATE_CONNECT &&
        data->mstate < CURLM_STATE_COMPLETED)
-      /* Make sure we set the connection's current owner */
+      /* Make sure we set the connection owner to the current handle,
+         a lot of code called from here depends on that. */
       data->easy_conn->data = data;
 
     if(data->easy_conn &&
